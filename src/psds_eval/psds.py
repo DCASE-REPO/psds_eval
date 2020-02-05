Fix class name handling in the confusion matrix calculation
A bug existed that was seen when class names appeared after the 'world'
label in a sorted set of class names. This then caused out of bounds
iteration errors when calculating the final confusion matrix.

This has been fixed by creating a sorted list of class names after having
removed the 'world' label. In addition, another list is obtained by
appending the 'world' label.

Test Plan:
To ensure that this bug was tested the class names in the
test_example_4 data have been adjusted so that they would appear after
the default 'world' label when sorted. The tests have also been
strengthened so that a more precise solution could be found.
All test pass with this new change.

PDM-156
Change-Id: I16ba855a775ba0659c2ef0df0bd78d0bd5700475

src/psds_eval/psds.py
@@ -284,27 +284,28 @@ class PSDSEval:
 
     def _evaluate_detections(self, tp, ct):
         """Produces a confusion matrix and detection rates for all classes"""
-        n_classes = len(self.class_names) - 1  # -1 to removes the world label
-        counts = np.zeros([n_classes + 1, n_classes + 1])
-        tp_ratio = np.zeros(n_classes)
-        fp_rate = np.zeros(n_classes)
-        ct_rate = np.zeros((n_classes, n_classes))
-        class_names_set = set(self.class_names)
+        n_real_classes = len(self.class_names) - 1 # don't count WORLD
+        counts = np.zeros([len(self.class_names), len(self.class_names)])
+        tp_ratio = np.zeros(n_real_classes)
+        fp_rate = np.zeros(n_real_classes)
+        ct_rate = np.zeros((n_real_classes, n_real_classes))
+        # Create an ordered set of class names without world
+        class_names_set_no_world = set(self.class_names).difference([WORLD])
+        # Create an ordered set of class names with world at the end
+        cls_names_world_end = sorted(class_names_set_no_world)
+        cls_names_world_end.append(WORLD)
         t_filter = self.ground_truth.event_label == WORLD
         dataset_dur = self.ground_truth[t_filter].duration.sum()
-        ct_tmp = \
-            ct.groupby(["event_label_det", "event_label_gt"]).filename.count()
+        ct_tmp = ct.groupby(["event_label_det",
+                             "event_label_gt"]).filename.count()
         gt_dur = self.ground_truth.groupby("event_label").duration.sum()
-
-        # counts is a confusion matrix
+        n_cls_gt = self.ground_truth.groupby("event_label").filename.count()
         # i, cls: detection -- j, ocls: ground truth
-        for i, cls in enumerate(sorted(class_names_set.difference([WORLD]))):
+        for i, cls in enumerate(sorted(class_names_set_no_world)):
             counts[i, i] = len(tp[tp.event_label_gt == cls])
-            n_cls_gt = \
-                self.ground_truth.groupby("event_label").filename.count()
             if cls in n_cls_gt:
                 tp_ratio[i] = counts[i, i] / n_cls_gt[cls]
-            for j, ocls in enumerate(sorted(class_names_set)):
+            for j, ocls in enumerate(cls_names_world_end):
                 try:
                     counts[j, i] = ct_tmp[cls, ocls]
                 except KeyError:

tests/test_psds_examples.py
@@ -32,9 +32,18 @@ def test_example_1_paper_icassp(metadata):
         [1, 0, 1, 0],
         [0, 0, 0, 0]
     ])
-
+    tpr = np.array([1., 1., 1.])
+    fpr = np.array([12.857143, 12.857143, 0.])
+    ctr = np.array([
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [720., 0., 0.]
+    ])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.9142857142857143), \
         "PSDS value was calculated incorrectly"
@@ -59,9 +68,18 @@ def test_example_2_paper_icassp(metadata):
         [0, 0, 1, 1],
         [0, 0, 0, 0]
     ])
-
+    tpr = np.array([0., 0., 1.])
+    fpr = np.array([12.857143, 0., 12.857143])
+    ctr = np.array([
+        [0., 0., 240.],
+        [144., 0., 144.],
+        [0., 0., 0.]
+    ])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.29047619047619044), \
         "PSDS value was calculated incorrectly"
@@ -88,9 +106,18 @@ def test_example_3_paper_icassp(metadata):
         [1, 0, 0, 1],
         [0, 0, 0, 0]
     ])
-
+    tpr = np.array([1., 1., 0.])
+    fpr = np.array([12.857143, 0., 12.857143])
+    ctr = np.array([
+        [0., 0., 0.],
+        [0., 0., 0.],
+        [600., 0., 0.]
+    ])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.6238095238095237), \
         "PSDS value was calculated incorrectly"
@@ -118,9 +145,19 @@ def test_example_4(metadata):
         [0, 0, 0, 0, 2],
         [0, 0, 0, 0, 0]
     ])
-
+    tpr = np.array([1., 0., 1., 0.])
+    fpr = np.array([0, 38.57142857, 0, 25.71428571])
+    ctr = np.array([
+        [0, 0, 0, 87.80487805],
+        [0, 0, 0, 300],
+        [0, 156.52173913, 0, 0],
+        [0, 0, 0, 0]
+    ])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.5), \
         "PSDS value was calculated incorrectly"
@@ -146,9 +183,14 @@ def test_det_on_file_no_gt():
         [0, 1],
         [0, 0]
     ])
-
+    tpr = np.array([0.])
+    fpr = np.array([360.])
+    ctr = np.array([[0.]])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.0), \
         "PSDS value was calculated incorrectly"
@@ -201,9 +243,14 @@ def test_empty_det():
         [0, 0],
         [0, 0]
     ])
-
+    tpr = np.array([0.])
+    fpr = np.array([0.])
+    ctr = np.array([[0.]])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.0), \
         "PSDS value was calculated incorrectly"
@@ -231,9 +278,17 @@ def test_files_from_dcase(metadata):
         [1., 4., 0.],
         [0., 0., 0.]
     ])
-
+    tpr = np.array([0.25, 1.])
+    fpr = np.array([12.857143,  0.])
+    ctr = np.array([
+       [0., 0.],
+       [600.40026684, 0.]
+    ])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     assert psds1.value == pytest.approx(0.6089285714285714), \
         "PSDS value was calculated incorrectly"
@@ -272,9 +327,39 @@ def test_full_dcase_validset():
         [0, 83, 1, 12, 58, 27, 46, 46, 120, 67, 390],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ])
-
+    tpr = np.array([0.64047619, 0.61458333, 0.37829912, 0.28924162, 0.4877193,
+                    0.63076923, 0.92553191, 0.53586498, 0.55074116,
+                    0.72826087])
+    fpr = np.array([93.08219178, 128.21917808, 180.30821918, 339.04109589,
+                    456.16438356, 118.97260274, 258.90410959, 204.04109589,
+                    413.01369863, 120.20547945])
+    ctr = np.array([
+        [0., 39.38051054, 275.66357376, 179.40010356, 525.07347382,
+         56.88295966, 30.62928597, 78.76102107, 560.07837208, 8.75122456],
+        [36.54377132, 0, 29.23501705,  328.89394185, 211.95387364,
+         226.57138217, 255.80639922, 336.20269612, 628.55286666, 423.90774728],
+        [412.06956854, 129.72560491, 0, 144.98744078, 801.24638326,
+         99.20193317, 106.8328511, 122.09468697, 625.73527074, 152.61835872],
+        [375.77227974, 436.70832511, 81.24806048, 0, 568.73642339, 91.40406805,
+         639.82847632, 639.82847632, 883.57265777, 71.09205292],
+        [201.60236547, 44.80052566, 353.92415271, 327.04383731, 0, 31.36036796,
+         107.52126158, 228.48268086, 689.92809516, 98.56115645],
+        [100.2921207, 157.60190396, 78.80095198, 171.92934977, 214.91168722, 0,
+         365.34986827, 186.25679559, 444.15082025, 308.04008501],
+        [13.91555321, 92.77035472, 55.66221283, 630.83841208, 445.29770265,
+         162.34812076, 0, 477.7673268, 449.93622038, 125.23997887],
+        [23.16073227, 118.69875286, 37.63618993, 344.51589244, 269.24351258,
+         138.96439359, 390.83735697, 0, 535.59193363, 92.64292906],
+        [122.13847545, 164.68109049, 101.55333914, 676.56481345, 1132.18249714,
+         278.58551142,  553.05399557, 256.62803269, 0., 122.13847545],
+        [0, 382.88155531, 4.61303079, 55.35636944, 267.55578564, 124.55183125,
+         212.1994162, 212.1994162, 553.56369442, 0]
+    ])
     psds_eval.add_operating_point(det)
     assert np.all(psds_eval.operating_points.counts[0] == exp_counts)
+    np.testing.assert_allclose(psds_eval.operating_points.tpr[0], tpr)
+    np.testing.assert_allclose(psds_eval.operating_points.fpr[0], fpr)
+    np.testing.assert_allclose(psds_eval.operating_points.ctr[0], ctr)
     psds1 = psds_eval.psds(0.0, 0.0, 100.0)
     # Check that all the psds metrics match
     assert psds1.value == pytest.approx(0.0044306914546640595), \
@@ -301,17 +386,14 @@ def test_multi_ops_multiple_times_sequentially():
     psds_eval = PSDSEval(dtc_threshold=0.5, gtc_threshold=0.5,
                          cttc_threshold=0.3, ground_truth=gt,
                          metadata=metadata)
-
+    ref_psds_value = 0.07211376135412327
     for k in range(3):
         for det_t in dets:
             psds_eval.add_operating_point(det_t)
-
         psds = psds_eval.psds(0.0, 0.0, 100)
-
-        if k == 0:
-            ref_psds_value = psds.value
-        else:
-            assert psds.value == pytest.approx(ref_psds_value), \
-                "PSDS was calculated incorrectly"
-
+        assert psds.value == pytest.approx(ref_psds_value), \
+            "PSDS was calculated incorrectly"
         psds_eval.clear_all_operating_points()
+        assert psds_eval.num_operating_points() == 0
+        assert len(psds_eval.operating_points) == 0
+